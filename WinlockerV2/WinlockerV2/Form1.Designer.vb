9dcb2cc56d53eb48f7ef1634666f4e7828d56e4f
rgamin1 <>
2018-03-28 18:14:18 -0700

    Color, Size, Window State, and More
    Line 21 size change
    Line 25 IDK
    Line 31-36 New BackColor, New Size, New icon, Border set to non.
    Line 33 Text the same
    Line 38 Name set to Winlocker V2
    Line 39 Maximised

--- WinlockerV2/WinlockerV2/Form1.Designer.vb
+++ WinlockerV2/WinlockerV2/Form1.Designer.vb
@@ -22,15 +22,21 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.SuspendLayout()
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(886, 475)
+        Me.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.ClientSize = New System.Drawing.Size(1277, 725)
+        Me.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "WinLocker V2"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
         Me.ResumeLayout(False)
 
     End Sub
